7799bceabdfa76974d6fb52aa97cbd3f43531292
Eisbaeri <maximilian@alexanderschardt.de>
2024-05-19 15:26:33 +0200

    Added Image if it has CC BY-SA 4.0 License.

--- articleExtractor.py
+++ articleExtractor.py
@@ -11,11 +11,52 @@ def searchArticles(language_code='en', search_query='Never gonna give you up'):
     articles = []
 
     for id, value in jsonResponse['query']['pages'].items():
+
+        img = getImage(language_code, value['title'])
+
         articles.append({
             'lang': language_code,
             'title': value['title'],
             'id': id,
-            'summary': value['extract']
+            'summary': value['extract'],
+            'picture': img
         })
 
     return articles
+
+def getImage(language_code='en', search_query='Never gonna give you up'):
+    url = f'https://{language_code}.wikipedia.org/w/api.php?action=query&prop=pageimages&format=json&piprop=original&titles={search_query}'
+    response = requests.get(url)
+    jsonResponse = response.json()
+
+    imgs = []
+
+    for id, value in jsonResponse['query']['pages'].items():
+        if 'original' in value:
+            imgs.append(value['original']['source'])
+        else:
+            imgs.append('noimg')
+
+    img = imgs[0]
+    if img != 'noimg':
+        if getLicense(img, language_code) == 'CC BY-SA 4.0':
+            return {'img': img, 'license': getLicense(img, language_code)}
+
+    return {'img': 'noimg', 'license': 'noimg'}
+
+def getLicense(imgURL, language_code='en'):
+
+    # query > pages > -1 > imginfo > 0 > extmetadata > LicenseShortName > value
+
+    imgName = imgURL.split('/')[-1]
+
+    url = f'https://{language_code}.wikipedia.org/w/api.php?action=query&prop=imageinfo&iiprop=extmetadata&format=json&titles=File%3a{imgName}'
+    response = requests.get(url)
+    jsonResponse = response.json()
+
+    if jsonResponse['query']['pages']['-1']['imageinfo'][0]['extmetadata']['LicenseShortName']['value'] == 'CC BY-SA 4.0':
+        return 'CC BY-SA 4.0'
+    else:
+        return 'noLicense'
+
+print(searchArticles(search_query='sun'))
